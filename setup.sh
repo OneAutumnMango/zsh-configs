51a81dbaa1e3511141844ac90862e82e191e923a
David Whittam <84255659+OneAutumnMango@users.noreply.github.com>
2025-09-04 22:58:38 +0100

    Update setup.sh removal

--- setup.sh
+++ setup.sh
@@ -3,15 +3,14 @@
 sudo apt update
 sudo apt install -y zsh fzf
 
-# script directory
-
 cp -r .config/ ~/.config
 cp .zsh* ~
 
+SCRIPT_DIR="$(cd -- "$(dirname -- "$0")" && pwd)"
 cd ~
 
 curl -L git.io/antigen > .antigen.zsh
 curl -sS https://starship.rs/install.sh | sh
 curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
 
-rm -rf "$(dirname "$(dirname "$0")")"
+rm -rf "$SCRIPT_DIR"

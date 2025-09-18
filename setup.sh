bc40ecbd9171e3613313eb5a7a80322a70b1de2c
OneAutumnMango <david.m.whittam@gmail.com>
2025-09-18 12:12:40 +0100

    rename master branch to main

--- setup.sh
+++ setup.sh
@@ -6,10 +6,10 @@ sudo apt update
 sudo apt install -y zsh fzf
 
 TMPDIR=$(mktemp -d)
-curl -L https://github.com/OneAutumnMango/zsh-configs/archive/refs/heads/master.tar.gz -o "$TMPDIR/zsh-configs.tar.gz"
+curl -L https://github.com/OneAutumnMango/zsh-configs/archive/refs/heads/main.tar.gz -o "$TMPDIR/zsh-configs.tar.gz"
 tar -xzf "$TMPDIR/zsh-configs.tar.gz" -C "$TMPDIR"
 
-REPO_DIR="$TMPDIR/zsh-configs-master"
+REPO_DIR="$TMPDIR/zsh-configs-main"
 
 cp "$REPO_DIR/.zsh*" ~/
 cp -r "$REPO_DIR/.config/" ~/.config

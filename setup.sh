0dce838be4e3b70ee6793584c2321e16958ccfc1
David Whittam <84255659+OneAutumnMango@users.noreply.github.com>
2025-09-18 11:11:28 +0100

    remove starship confirmation

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ SCRIPT_DIR="$(cd -- "$(dirname -- "$0")" && pwd)"
 cd ~
 
 curl -L git.io/antigen > .antigen.zsh
-curl -sS https://starship.rs/install.sh | sh
+curl -sS https://starship.rs/install.sh | sh -s -- -y
 curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
 
 rm -rf "$SCRIPT_DIR"
